50aefc9940b12af82a8c78ea6c9d3a7445c4d0d5
Vanya Usalko <dk4aol@gmail.com>
2024-07-01 23:46:02 +0300

    # There is a autopep8 dependency # There is the test for covariance/contravariance TypeVar

--- src/example999.py
+++ src/example999.py
@@ -1,10 +1,60 @@
 # example999.py
 
-from typing import TypeVar
+from typing import Generic, TypeVar
+
+T_co = TypeVar('T_co', covariant=True)
+T_contra = TypeVar('T_contra', contravariant=True)
 
 
-T_co=TypeVar('T_co', covariant=True)
-T_contra=TypeVar('T_contra', contravariant=True)
+class I_co(Generic[T_co]):
+    t: T_co
+
+    def __init__(self, t: T_co) -> None:
+        super().__init__()
+        self.t = t
+
+
+class I_contra(Generic[T_contra]):
+    t: T_contra
+
+    def __init__(self, t: T_contra) -> None:
+        super().__init__()
+        self.t = t
+
+
+class I_co_derived(I_co):
+
+    def print(self) -> None:
+        print(f'{self.t}')
+
+
+class I_contra_derived(I_contra):
+
+    def print(self) -> None:
+        print(f'{self.t}')
+
+
+class A:
+    a: int = 0
+
+    def __str__(self) -> str:
+        return f'{self.a}'
+
+
+class B(A):
+    b: int = 0
+
+    def __str__(self) -> str:
+        return f'{self.a},{self.b}'
+
+
+def print_co(i: I_co[A]) -> None:
+    print(f'{i.t}')
+
+
+def print_contra(i: I_contra[B]) -> None:
+    print(f'{i.t}')
+
 
 def example999(input: str):
     '''
--- tests/test_example999.py
+++ tests/test_example999.py
@@ -3,15 +3,31 @@
 '''
 
 import unittest
-from src import example999
+from src.example999 import *
+
 
 class TestExample999(unittest.TestCase):
 
     def test_example999(self):
         self.assertEqual(
-            example999(),
-            '3.12',
-		)
+            example999(''),
+            '',
+        )
+
+    def test_covariance(self):
+        a = A()
+        b = B()
+
+        print_co(I_co(a))
+        print_co(I_co(b))
+
+    def test_contravariant(self):
+        a = A()
+        b = B()
+
+        print_contra(I_contra(a))
+        print_contra(I_contra(b))
+
 
 if __name__ == '__main__':
     unittest.main()
